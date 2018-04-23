ef1a3ec301efb1d2b8950d6959ab0c8fd1d3d27b
miglee <mignionlee@gmail.com>
2018-04-22 23:54:38 -0400

    1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     ## print the guestbook
-    return render_template("index.html", entries=model.get_entries())
+    return render_template("admin.html", entries=model.get_entries())
 
 @app.route("/add")
 def addentry():
@@ -18,6 +18,12 @@ def postentry():
     name = request.form["name"]
     message = request.form["message"]
     model.add_entry(name, message)
+    return redirect("/")
+
+@app.route("/delete")
+def delete():
+    id_ = request.form["id"]
+    model.delete_entry(id_)    
     return redirect("/")
 
 if __name__=="__main__":
--- model.py
+++ model.py
@@ -4,14 +4,26 @@ from datetime import datetime
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+next_id = 0
 
-def init(app):
+def init():
     global entries
+    global next_id
     try:
 
         f = open(GUESTBOOK_ENTRIES_FILE)
         entries = json.loads(f.read())
         f.close()
+        id_list = []
+        for a in entries:
+            is_list.append(a['id'])
+        max_id = 0
+        for a in is_list:
+            if a > max_id:
+                max_id=a
+        next_id = max_id+1
+
+
     except:
         print('Couldn\'t open', GUESTBOOK_ENTRIES_FILE)
         entries = []
@@ -20,11 +32,12 @@ def get_entries():
     global entries
     return entries
 
-def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+def add_entry(name, text,):
+    global entries, GUESTBOOK_ENTRIES_FILE,next_id
     now = datetime.now()
-    time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    time_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    entry = {"author": name, "text": text, "timestamp": time_string, "id": str(next_id)}
+    next_id +=1
     entries.insert(0, entry) ## add to front of list
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
@@ -33,3 +46,20 @@ def add_entry(name, text):
         f.close()
     except:
         print("ERROR! Could not write entries to file.")
+
+
+def delete_entry(id_):
+    global entries, GUESTBOOK_ENTRIES_FILE
+    for entry in entries:
+###==
+        if entry['id'] == id_:
+                del entry['id']
+
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        dump_string = json.dumps(entries)
+        f.write(dump_string)
+        f.close()
+    except:
+        print("ERROR! Could not write entries to file.")
+
